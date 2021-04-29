8275340215ff0385244a5282db44152a1a5db77a
Xenvex <matthewbreyes@gmail.com>
2021-04-29 13:00:59 -0700

    PlantListUpdate
    -Current Parameters:
    plantListID(AutoGenerated)
    userID(Id of user who made the list)
    plantListName(Name of the List)
    Plants(List of Plants)
    -Currently no direct method of inserting plants into the list
    -Uncertain of how to properly store plants(Because of Firestore Interactions)

--- PlantFinder/app/src/main/java/yourid/csumb/plantfinder/model/PlantList.java
+++ PlantFinder/app/src/main/java/yourid/csumb/plantfinder/model/PlantList.java
@@ -2,31 +2,47 @@ package yourid.csumb.plantfinder.model;
 
 
 import androidx.room.Entity;
+import androidx.room.PrimaryKey;
 
 import java.io.Serializable;
+import java.util.List;
 
 @Entity(tableName = PlantListDatabase.PL_TABLE)
 public class PlantList implements Serializable {
+
+    @PrimaryKey(autoGenerate = true)
+    private int plantListId;
+
     private int userID;
 
-    private String plantName;
+    private String plantListName;
+    private List<String> Plants; //List of Plants in the List
 
-    public PlantList(int userID, String plantName){
+    public PlantList(int userID, String plantListName, List<String> Plants){
         this.userID = userID;
-        this.plantName = plantName;
+        this.plantListName = plantListName;
+        this.Plants = Plants;
     }
+
+    public int getPlantListId(){ return plantListId; }
+
+    public List<String> getPlants(){ return Plants;}
 
     public int getUserId() {
         return userID;
     }
 
+    public String getPlantListName(){ return plantListName; }
+
+    public void setPlantListId(int plantListId){ this.plantListId = plantListId; }
+
+    public void setPlants(List<String> plants){ this.Plants = plants; }
+
     public void setUserId(int userID) {
         this.userID = userID;
     }
 
-    public String getPlantName(){ return plantName; }
-
-    public void setPlantName(String plantName) {
-        this.plantName = plantName;
+    public void setPlantName(String plantListName) {
+        this.plantListName = plantListName;
     }
 }
